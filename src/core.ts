e5a427ebaa47111837197982505815fc3f84070a
Matteo Zanoncello <matteozanoncello.work@gmail.com>
2018-11-06 21:20:42 +0100

    code refactoring

--- src/core.ts
+++ src/core.ts
@@ -1,12 +1,11 @@
 import * as Emotive from 'emotive';
 import filterInvalidDOMProps from 'filter-invalid-dom-props';
-import {ComponentClass, createElement} from 'react';
+import {ComponentClass, createElement, ReactDOM} from 'react';
 
 export type Param = Emotive.Param;
 export type Sheet = Emotive.Sheet;
 
 export const Css = Emotive.Css;
-export const sheet = Css.sheet;
 export const Method = Emotive.Method;
 export const Length = Emotive.Length;
 export const Angle = Emotive.Angle;
@@ -18,20 +17,31 @@ export const Color = Emotive.Color;
 export const Unit = Emotive.Unit;
 export const Property = Emotive.Property;
 
-type PropsType = { [name: string]: any };
-type SheetAsArrow = (name: PropsType) => Sheet | Sheet[]
-export type PropertyType = Sheet | SheetAsArrow;
+type Props = { [name: string]: any };
+type MQ = string;
+type StyleParam = Sheet | SheetFromProps | SheetFromMQ;
 
-function component(elementType: string | ComponentClass, ...properties: PropertyType[]) {
-    return (props: { [name: string]: any }) => {
+type SheetFromProps = (props: Props) => Sheet | Sheet[];
+type SheetFromMQ = (mq: MQ) => Sheet | Sheet[];
+
+// export const Media = (query: string) => {
+//     return (window: Window) => {
+//         return (...props: StyleParam[]) => {
+//             return render ? props : {};
+//         }
+//     };
+// };
+
+function component(elementType: string | ComponentClass, ...sheetProps: StyleParam[]) {
+    return (props: Props) => {
 
         let {style, ...otherProps} = props;
-
         let styleProps: Sheet[] = [];
-        properties.map(property => {
-            const isFunction = property && {}.toString.call(property) === '[object Function]';
-            if (isFunction) {
-                const computedProperty = (<SheetAsArrow>property)(props);
+
+        sheetProps.map(sheet => {
+            // function
+            if (sheet && {}.toString.call(sheet) === '[object Function]') {
+                const computedProperty = (<SheetFromProps>sheet)(props);
                 if (Array.isArray(computedProperty)) {
                     styleProps.push(...<Sheet[]>computedProperty);
                 }
@@ -39,15 +49,19 @@ function component(elementType: string | ComponentClass, ...properties: Property
                     styleProps.push(<Sheet>computedProperty);
                 }
             }
+            else if (sheet instanceof Media) {
+
+            }
+            // Sheet
             else {
-                styleProps.push(<Sheet>property);
+                styleProps.push(<Sheet>sheet);
             }
         });
         if (style) {
             styleProps.push(style);
         }
 
-        const emotiveStyle = sheet(...styleProps);
+        const emotiveStyle = Css.sheet(...styleProps);
         const domProps = filterInvalidDOMProps(otherProps);
         return createElement<any>(elementType, {style: emotiveStyle, ...domProps}, props.children);
     }
